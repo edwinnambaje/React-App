4bbf687312d70663beadd041a5bf9d5a0252e1ed
edwinnambaje <nambajeeedwin@gmail.com>
2023-02-17 16:14:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+function App() {
+  return (
+    <div>
+      <h1>My Todo</h1>
+      <Todo text="Edwin"/>
+      <Todo text="Alu"/>
+      <Todo text="Elu"/>
+      <Todo text="ALX"/>
+    </div>
+  );
+}
+
+export default App;
