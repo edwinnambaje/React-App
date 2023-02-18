1e263e8342c78da48985b46724f34d84ccbc5ccb
edwinnambaje <nambajeeedwin@gmail.com>
2023-02-19 01:47:53 +0200

    completed rendering all meetups

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,4 +1,21 @@
+import NewMeetupForm from "../components/meetups/NewMeetup";
+import { useNavigate } from "react-router";
 function NewMeetups(){
-    return <div>Add NewMeetups Page</div>;
+    const navigate=useNavigate()
+    function addMeetup(meetupdata){
+        fetch("https://react-49783-default-rtdb.firebaseio.com/meetups.json",{
+            method:"POST",
+            body:JSON.stringify(meetupdata),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        }).then(()=>{
+            navigate('/')
+        })
+    }
+    return<div>
+        <h1>Add New Meet up</h1>
+        <NewMeetupForm onAddMeetups={addMeetup}/>
+    </div>
 }
 export default NewMeetups;
