2f342b3f393b12997caed01a7a5f620745c8d8f9
edwinnambaje <nambajeeedwin@gmail.com>
2023-02-20 00:24:03 +0200

    added login and contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,30 @@
+import './contact.css'
 function Contact(){
-    return <h1>Contact Page</h1>;
+    return <section id="contact" className="contact">
+         <div className="contact-items">  
+          <div className="contact-info"><h1>Get In Touch With Me</h1>  
+          <div className="contact-form">
+            <div id="contact-form">
+              <div className="form-control">
+                <label htmlFor="name">Full Name</label>
+                <input type="text" id="name" name="sender-name" placeholder="Enter Your Full Name" className="input-field " />
+              </div>
+              <div className="name_error"> </div>
+              <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="sender-email" placeholder="Enter Your Email" className="input-field "/>
+              </div>
+              <div className="email_error"> </div>
+              <div className="form-control">
+                <label htmlFor="message">Message</label>
+                <textarea id="message" cols="70" rows="10" placeholder="Enter Your Message" name="message" className="input-field"></textarea>
+              </div>
+              <div className="message_error"> </div>
+              <div className="submit-button"><input type="submit" value="Submit" id="submit-btn" className="submit-btn"/></div>
+            </div>
+          </div>
+      </div>
+      </div>
+  </section>
 }
 export default Contact;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,15 @@
+import './Login.css'
 function Login(){
-    return <h1>Login Page</h1>;
+    return <div className="main">  	
+    <form  id="form_login" method="post" name="form">
+        <label>Signin</label>
+        <input type="text" id="input_email" name="email" placeholder="email" className="username"/>
+        <div className="email_error"></div>
+        <input type="password" id="input_password" name="password" placeholder="Password" className="pass password" />
+        <div className="password_error"></div>
+        <div className="form_error"></div>
+        <button type="submit" className="button">Signin</button>    
+    </form>
+</div>;
 }
 export default Login;
