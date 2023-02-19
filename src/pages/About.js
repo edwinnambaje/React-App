cdfa2d7e76ccbb2f9679a572a7c56d974d3576e1
edwinnambaje <nambajeeedwin@gmail.com>
2023-02-19 22:18:49 +0200

    remodified it to brand

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,22 @@
+import classes from './About.module.css'
+import profile from '../images/profile.jpg'
 function About(){
-    return <h1>About Page</h1>;
+    return <section className={classes.about}>
+        <div className={classes.abouts}>
+          <div className={classes.col}>
+            <div className={classes.img}>
+                <img src={profile} alt='edwin' width={270} height={360}/>
+            </div>
+          </div>
+          <div className={classes.cols}>
+            <h1 className={classes.title}>About <span>me</span></h1>
+            <h2>Software Engineer</h2>
+            <p>I'm an experienced software engineer who constantly seeks<br/>
+              out innovative solutions to everyday problems. In my five<br/>
+              years in this industry, I've honed my analytical thinking and<br/>
+              collaboration skills, and I love working with a team.</p>
+          </div>
+        </div>
+      </section>
 }
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,13 @@ import classes from './Meet.module.css';
 
 function Home(){
     return (
-    <section className={classes.hero}>
-    <div className={classes.heros}>
-       <div>
+    <section id={classes.hero}>
+    <div className={classes.hero}>
+    <div className={classes.overlay}>
         <h1>Hello, <span></span></h1>
         <h1>My Name is <span></span></h1>
         <h1>Edwin <span></span></h1>
-        {/* //<a href="/html/contact.html" type="button" class="cta">Hire Me</a> */}
-      </div>
+    </div>
     </div>
     </section>
     );
